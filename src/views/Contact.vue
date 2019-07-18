0be978c66ed26a4a46de4f5acc0ab8374866ab88
Eduardo Barron <eduard_park.13@hotmail.com>
2019-07-17 22:41:22 -0500

    columnas acomodadas

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,22 +5,22 @@
                     <h1 class="mt-5">Comienza ya tu proyecto!</h1>
                 </b-col>
             
-                <b-col cols="6">
+                <b-col cols="12" sm="12" md="6" lg="6">
                     <b-row>
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi aperiam dignissimos sed rem perferendis eveniet at similique ea tempora inventore, ipsum dolorem illo repudiandae laboriosam optio magni, minima vitae debitis.
                     </b-row>
                 </b-col>
 
-                <b-col cols="6">
+                <b-col cols="12" sm="12" md="6" lg="6">
                     <b-row>
-                        <b-col cols="6">
+                        <b-col cols="6" sm="6" md="6" lg="6">
                             <b-cols cols="6" class="pl-0 d-flex align-content=start">
                                 <small>Nombre</small>
                             </b-cols>
                             <b-form-input placeholder="Tú nombre"></b-form-input>
                         </b-col>
 
-                        <b-col cols="6">
+                        <b-col cols="6" sm="6" md="6" lg="6">
                             <b-cols cols="6" class="pl-0 d-flex align-content=start">
                                 <small>Telefono</small>
                             </b-cols>
